83a31cffb213bdb7fb1f7414b055ce6fc786ec2e
guram kasradze <guramkasradze123@gmail.com>
2020-11-11 20:08:10 +0400

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ProductsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await _dbContext.Products.ToListAsync()); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            return Ok(await _dbContext.Products.FindAsync(id));
+        }
+    }
+}
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
