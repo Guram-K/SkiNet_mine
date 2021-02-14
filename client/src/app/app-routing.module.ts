c0162c040a56c0e8329f2c3e3545ce52808d073d
guram kasradze <guramkasradze123@gmail.com>
2021-02-14 14:22:04 +0400

    small fixes client side

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { ShopComponent } from './shop/shop.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule) },
-  { path: '**', redirectTo: '', pathMatch: 'full'}
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule) },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
